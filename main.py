aac52fd967f6a1ef7bd2438b20126e5113f6dc8f
Acrolein7777 <119024123+Acrolein7777@users.noreply.github.com>
2023-06-17 18:30:06 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def randomString(rolls):
+    data = "qwertyuioplkjhgfdsazxcvbnm1234567890QWERTYUIOPLKJHGFDSAZXCVBNM"
+    result = ""
+    while rolls >= 1:
+        c = random.choice(data)
+        result = c + result
+        rolls = rolls - 1
+    return result
